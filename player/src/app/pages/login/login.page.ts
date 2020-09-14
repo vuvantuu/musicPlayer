cf2a2992f4a6576e8bb4177fab22a00e6c0ae891
Vũ Tựu <vu.van.tuu@rue.co.jp>
2020-09-14 16:29:24 +0700

    login signup

---
+++ player/src/app/pages/login/login.page.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthConstants } from '../../config/auth-constants';
+import { AuthService } from './../../services/auth.service';
+import { StorageService } from './../../services/storage.service';
+import { ToastService } from './../../services/toast.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
+})
+export class LoginPage implements OnInit {
+  postData = {
+    email: '',
+    password: ''
+  };
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private storageService: StorageService,
+    private toastService: ToastService
+  ) {}
+
+  ngOnInit() {}
+
+  validateInputs() {
+    console.log(this.postData);
+    let email = this.postData.email.trim();
+    let password = this.postData.password.trim();
+    return (
+      this.postData.email &&
+      this.postData.password &&
+      email.length > 0 &&
+      password.length > 0
+    );
+  }
+
+  loginAction() {
+    if (this.validateInputs()) {
+     console.log("Debug data : ", this.postData)
+      this.authService.login(this.postData).subscribe(
+        (res: any) => {
+          if (parseInt(res.result.code) == 0)  {
+            // Storing the User data.
+            this.storageService
+              .store(AuthConstants.AUTH, res.result.data.token)
+              .then(res => {
+                this.router.navigate(['home']);
+              });
+          } else {
+            this.toastService.presentToast('Incorrect username and password.');
+          }
+          // console.log(JSON.stringify(res))
+        },
+        (error: any) => {
+          this.toastService.presentToast('Network Issue.');
+        }
+      );
+    } else {
+      this.toastService.presentToast(
+        'Please enter email/username or password.'
+      );
+    }
+  }
+}
---
+++ player/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // apiUrl: 'https://musicplayerionic.000webhostapp.com/Backend/public/index.php/api'
+  apiUrl: 'https://chatbot-demo.smart2j.jp/api/'
+};
