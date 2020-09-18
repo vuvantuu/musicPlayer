e1f24f5cf235504dbe2bc5212f600558d0436882
Vũ Tựu <vu.van.tuu@rue.co.jp>
2020-09-18 13:41:48 +0700

    two option

--- player/src/app/app.module.ts
+++ player/src/app/app.module.ts
@@ -9,9 +9,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StreamingMedia, StreamingVideoOptions } from '@ionic-native/streaming-media/ngx'; 
 import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player/ngx';
-
+import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/camera/ngx';
 import { Media } from '@ionic-native/media/ngx';
-
+import { File } from '@ionic-native/File/ngx';
+import { IonicStorageModule } from '@ionic/storage';
+import { FilePath } from '@ionic-native/file-path/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -19,13 +21,17 @@ import { Media } from '@ionic-native/media/ngx';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    IonicStorageModule.forRoot()
   ],
   providers: [
     StatusBar,
     SplashScreen,
     Media,
     HttpClientModule,
+    Camera,
+    FilePath,
+    File,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     StreamingMedia, YoutubeVideoPlayer
   ],
--- player/src/app/pages/settings/settings.page.ts
+++ player/src/app/pages/settings/settings.page.ts
@@ -1,17 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './../../services/auth.service';
+import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/camera/ngx';
+import { ActionSheetController, ToastController, Platform, LoadingController } from '@ionic/angular';
+import { File, FileEntry } from '@ionic-native/File/ngx';
+import { Storage } from '@ionic/storage';
+import { FilePath } from '@ionic-native/file-path/ngx';
+
+import { finalize } from 'rxjs/operators';
+
+const STORAGE_KEY = 'my_images';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss']
 })
-export class SettingsPage implements OnInit {
-  constructor(private authService: AuthService) {}
 
+export class SettingsPage implements OnInit {
+  images = [];
+  myPhoto:string = "";
+  constructor(private authService: AuthService, private camera: Camera, private file: File, 
+    private actionSheetController: ActionSheetController, private toastController: ToastController,
+    private storage: Storage, private plt: Platform, private loadingController: LoadingController, private filePath: FilePath) {}
+ 
   ngOnInit() {}
 
   logoutAction() {
     this.authService.logout();
   }
+  async getPic() {
+    const actionSheet = await this.actionSheetController.create({
+      header: "Select Image source",
+      buttons: [{
+              text: 'Load from Library',
+              handler: () => {
+                  this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+              }
+          },
+          {
+              text: 'Use Camera',
+              handler: () => {
+                  this.takePicture(this.camera.PictureSourceType.CAMERA);
+              }
+          },
+          {
+              text: 'Cancel',
+              role: 'cancel'
+          }
+      ]
+  });
+  await actionSheet.present();
+  }
+  takePicture(sourceType: PictureSourceType) {
+    var options: CameraOptions = {
+        quality: 100,
+        sourceType: sourceType,
+        saveToPhotoAlbum: false,
+        correctOrientation: true
+    };
+ 
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.myPhoto = base64Image;
+     }, (err) => {
+      // Handle error
+     });
+ 
+  }
+
 }
